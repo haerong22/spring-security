2de48ffa975913ae12e0303b1523e7e5c2ed85d3
haerong22 <haerong22@naver.com>
2021-05-29 18:53:51 +0900

    create user

---
+++ server/basic-test/src/main/java/com/study/springsecurity/web/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.study.springsecurity.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true )
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * user 생성
+     * - username, password, role 설정
+     *
+     * password 는 encode 하지 않으면 오류 발생
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(User.builder()
+                        .username("user1")
+                        .password(passwordEncoder().encode("1234"))
+                        .roles("USER"))
+                .withUser(User.builder()
+                        .username("admin")
+                         .password(passwordEncoder().encode("1234"))
+                        .roles("ADMIN"));
+    }
+
+    /**
+     * passwordEncoder 설정
+     */
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * security 설정
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+}

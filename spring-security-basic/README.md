735a9c404d5b1ae7cc5db869db07c52febbfefeb
haerong22 <haerong22@naver.com>
2021-10-27 02:26:57 +0900

    spring security basic : remember me

--- spring-security-basic/README.md
+++ spring-security-basic/README.md
@@ -3,4 +3,7 @@
 - Form Login
   - Form Login Authentication Filter (UsernamePasswordAuthenticationFilter)
   - Logout (LogoutFilter)
-    
+  
+
+- Remember Me
+  - RememberMeAuthenticationFilter
--- spring-security-basic/src/main/java/com/example/springsecuritybasic/config/SecurityConfig.java
+++ spring-security-basic/src/main/java/com/example/springsecuritybasic/config/SecurityConfig.java
@@ -4,19 +4,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -55,6 +55,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     response.sendRedirect("/login");
                 })
                 .deleteCookies("JSESSIONID", "remember-me")  // 쿠키 삭제
+                ;
+
+        http
+                .rememberMe()
+                .rememberMeParameter("remember-me") // 파라미터 name 변경 (기본 값은 remember-me)
+                .tokenValiditySeconds(3600)  // 만료 기간 (기본 값 14일)
+                .userDetailsService(userDetailsService)  // remember me 인증 시 유저계정 조회 시 필요한 클래스
                 ;
 
     }

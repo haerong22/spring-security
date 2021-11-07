784eecf5f1dab502d788b14117c509586831acb0
haerong22 <haerong22@naver.com>
2021-11-08 02:31:16 +0900

    spring security admin : init

---
+++ spring-security-admin/src/main/java/com/example/springsecurityadmin/SpringSecurityAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAdminApplication.class, args);
+    }
+
+}
